2f71478c3e72c32ed876d90c33d41b245939ffcc
imamulahsan <imamulahsan@hotmail.com>
2023-09-03 12:28:33 +0200

    frontend created

---
+++ client/src/components/Layout/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Header
