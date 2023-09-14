c09d98067151e3f1d67aa6a1f96ec7adcfa89b63
imamulahsan <imamulahsan@hotmail.com>
2023-09-14 22:15:35 +0200

    login design completed

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Form, Input, message } from "antd";
 import { Link, useNavigate } from "react-router-dom";
+import {EuroOutlined} from '@ant-design/icons';
 import axios from "axios";
 import Spinner from "../components/Spinner";
 const Login = () => {
@@ -32,10 +33,11 @@ const Login = () => {
   }, [navigate]);
   return (
     <>
-      <div className="resgister-page ">
+      <div className="login-page ">
         {loading && <Spinner />}
-        <Form layout="vertical" onFinish={submitHandler}>
-          <h1>Login Form</h1>
+        <Form  className="login-form" layout="vertical" onFinish={submitHandler}>
+          <h4>Personal <span> EXPENSE TRACKER </span><EuroOutlined/></h4>
+          <p>Log in to view your personal expenses:</p>
 
           <Form.Item label="Email" name="email">
             <Input type="email" />
@@ -43,10 +45,13 @@ const Login = () => {
           <Form.Item label="Password" name="password">
             <Input type="password" />
           </Form.Item>
-          <div className="d-flex justify-content-between">
-            <Link to="/register">Not a user ? Cleck Here to regsiter</Link>
-            <button className="btn btn-primary">Login</button>
+          <div className="login-button">
+            <button className="btn btn-secondary">Login</button>
           </div>
+          <div className="login-register">
+            <Link to="/register">New user? Click Here to Register</Link>
+          </div>
+         
         </Form>
       </div>
     </>
